4c42a1230a6db87130dcd3d85065521702f6f872
Akinola Abiodun E <akinolaemmanuel49@gmail.com>
2021-07-29 09:07:46 +0000

    need to write tests

--- minimart/products/routes.py
+++ minimart/products/routes.py
@@ -5,11 +5,17 @@ from minimart.models import Product
 from minimart.products import products
 
 
-@products.route('/list')
+@products.route('/explore')
 def product_list():
     title = 'Minimart - Products'
     year = datetime.utcnow().year
     products = Product.query.all()
-    return render_template('products/products_list.html', title=title, products=products, year=year)
+    return render_template('products/products_list.html', title=title, year=year, products=products)
 
-# @products.route('/<id>')
+
+@products.route('/<int:product_id>')
+def product_view(product_id):
+    product = Product.query.filter_by(id=product_id).first_or_404()
+    title = f'Minimart - {product.name}'
+    year = datetime.utcnow().year
+    return render_template('products/product_view.html', product=product, title=title, year=year)
