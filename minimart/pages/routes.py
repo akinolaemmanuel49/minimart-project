64783991cf3c45cf38552982548876b51c29a09c
KrazyKahunaGuy <54121054+KrazyKahunaGuy@users.noreply.github.com>
2021-08-18 11:07:31 +0100

    Checking for current_user

--- minimart/pages/routes.py
+++ minimart/pages/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask import render_template
+from flask_login import current_user
 
 from minimart.pages import pages
 
@@ -18,3 +19,11 @@ def about_us():
     title = 'Minimart - About us'
     year = datetime.utcnow().year
     return render_template('pages/about-us.html', title=title, year=year)
+
+
+@pages.route('/get_current_user', methods=['GET'])
+def get_current_user():
+    title = 'REMOVE LATER'
+    year = datetime.utcnow().year
+    username = current_user.username
+    return render_template('pages/get_current_user.html', title=title, year=year, username=username)
