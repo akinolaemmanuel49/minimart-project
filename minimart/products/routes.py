2bdf985b5a68c92e0af10d7811e36b6afce3c7fb
KrazyKahunaGuy <54121054+KrazyKahunaGuy@users.noreply.github.com>
2021-08-18 15:25:54 +0100

    made changes to products views

--- minimart/products/routes.py
+++ minimart/products/routes.py
@@ -8,6 +8,9 @@ from minimart import csrf
 from minimart.models import db, Category, Product
 from minimart.products import products
 
+def get_category(id):
+    category = Category.query.filter_by(id=id).first()
+    return category
 
 @products.route('/category/explore', methods=['GET'])
 def category_list():
@@ -25,7 +28,7 @@ def products_list():
     products = Product.query.order_by(Product.created.desc()).paginate(page, current_app.config['ITEMS_PER_PAGE'], False)
     next_view = url_for('products.products_list', page=products.next_num) if products.has_next else None
     prev_view = url_for('products.products_list', page=products.prev_num) if products.has_prev else None
-    return render_template('products/products_list.html', title=title, year=year, products=products.items, next_view=next_view, prev_view=prev_view)
+    return render_template('products/products_list.html', title=title, year=year, products=products.items, next_view=next_view, prev_view=prev_view, get_category=get_category)
 
 
 @products.route('/<int:product_id>', methods=['GET'])
@@ -33,7 +36,7 @@ def product_view(product_id):
     product = Product.query.filter_by(id=product_id).first_or_404()
     title = f'Minimart - {product.name}'
     year = datetime.utcnow().year
-    return render_template('products/products_view.html', product=product, title=title, year=year)
+    return render_template('products/products_view.html', product=product, title=title, year=year, get_category=get_category)
 
 
 @csrf.include
