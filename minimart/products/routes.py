8423042b16821d0dc1f50d073045ffae21730e58
KrazyKahunaGuy <54121054+KrazyKahunaGuy@users.noreply.github.com>
2021-08-19 09:33:26 +0100

    Updated links in template views.

--- minimart/products/routes.py
+++ minimart/products/routes.py
@@ -12,12 +12,23 @@ def get_category(id):
     category = Category.query.filter_by(id=id).first()
     return category
 
-@products.route('/category/explore', methods=['GET'])
-def category_list():
+@products.route('/categories/explore', methods=['GET'])
+def categories_list():
     title = 'Minimart - Categories'
     year = datetime.utcnow().year
     categories = Category.query.all()
     return render_template('products/categories_list.html', title=title, year=year, categories=categories)
+
+@products.route('/category/<category_name>/explore', methods=['GET'])
+def category_explore(category_name):
+    title = 'Minimart - {}'.format(category_name)
+    year = datetime.utcnow().year
+    page = request.args.get('page', 1, type=int)
+    category = Category.query.filter_by(name=category_name).first()
+    products = Product.query.filter_by(category=category.id).order_by(Product.created.desc()).paginate(page, current_app.config['ITEMS_PER_PAGE'], False)
+    next_view = url_for('products.category_explore', category_name=category_name, page=products.next_num) if products.has_next else None
+    prev_view = url_for('products.category_explore', category_name=category_name, page=products.prev_num) if products.has_prev else None
+    return render_template('products/explore_product_by_category.html', title=title, year=year, products=products.items, next_view=next_view, prev_view=prev_view)
 
 
 @products.route('/explore', methods=['GET'])
