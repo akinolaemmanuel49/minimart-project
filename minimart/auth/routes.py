1f1e310394f3d08a223c53c5b62ff93474d37744
KrazyKahunaGuy <54121054+KrazyKahunaGuy@users.noreply.github.com>
2021-10-03 12:20:42 +0100

    minor changes...

--- minimart/auth/routes.py
+++ minimart/auth/routes.py
@@ -14,8 +14,10 @@ from werkzeug.urls import url_parse
 @csrf.include
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+# def signup(email="", username="", error_message="", password_is_invalid="false", confirm_password_is_invalid="false", username_is_invalid="false", email_is_invalid="false"):
     title = 'Minimart - Signup'
     year = datetime.utcnow().year
+
     if current_user.is_authenticated:
         return redirect(url_for('pages.home'))
     if request.method == 'POST':
@@ -23,30 +25,41 @@ def signup():
         username = request.form.get('username')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
-        print(confirm_password)
+        user = User(email=email, username=username)
+        if User.query.filter_by(email=user.email).first():
+            # email_is_invalid, error_message = "true", "This email address is already in use."
+            # return redirect(url_for('auth.signup', email_is_invalid=email_is_invalid, error_message=error_message))
+            return redirect(url_for('auth.signup'))
+        if User.query.filter_by(username=user.username).first():
+            print(User.query.filter_by(username=user.username).first())
+            # username_is_invalid, error_message = "true", "This username has already in user, please choose another."
+            # return redirect(url_for('auth.signup', username_is_invalid=username_is_invalid, error_message=error_message))
+            return redirect(url_for('auth.signup'))
         if len(password) < 8:
-            flash('Password must have atleast 8 characters.', 'error')
+            # password_is_invalid, error_message = "true", "Password must have atleast 8 characters."
+            # return redirect(url_for('auth.signup', email=email, username=username, password_is_invalid=password_is_invalid, error_message=error_message))
             return redirect(url_for('auth.signup'))
         if len(password) > 32:
-            flash('Password cannot exceed 32 characters.', 'error')
+            # password_is_invalid, error_message = "true", "Password cannot exceed 32 characters."
+            # return redirect(url_for('auth.signup', email=email, username=username, password_is_invalid=password_is_invalid, error_message=error_message))
             return redirect(url_for('auth.signup'))
         if password == confirm_password:
-            verified_password = password
+            user.set_password(password)
         else:
-            flash('Passwords do not match.', 'error')
+            # confirm_password_is_invalid, error_message = "true" ,"Passwords do not match."
+            # return redirect(url_for('auth.signup', email=email, username=username, confirm_password_is_invalid=confirm_password_is_invalid, error_message=error_message))
             return redirect(url_for('auth.signup'))
-        user = User(email=email, username=username)
-        user.set_password(verified_password)
+
         db.session.add(user)
         db.session.commit()
         flash('You have successfully signed up', 'message')
-        return redirect(url_for('auth.signin'))
+        return redirect(url_for('auth.signin', email=email))
     return render_template('auth/signup.html', title=title, year=year)
 
 
 @csrf.include
 @auth.route('/signin', methods=['GET', 'POST'])
-def signin():
+def signin(email=""):
     title = 'Minimart - Signin'
     year = datetime.utcnow().year
     if current_user.is_authenticated:
@@ -62,7 +75,7 @@ def signin():
             if not next_page or url_parse(next_page).netloc != '':
                 next_page = url_for('pages.home')
             return redirect(next_page)
-    return render_template('auth/signin.html', title=title, year=year)
+    return render_template('auth/signin.html', title=title, year=year, email=email)
 
 
 @auth.route('/signout')
--- minimart/dashboard/routes.py
+++ minimart/dashboard/routes.py
@@ -1,9 +1,9 @@
 from minimart.dashboard import dashboard
 from flask import render_template, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 @dashboard.route('')
 @login_required
 def main():
-    return 'DASHBOARD'
+    return current_user.username
